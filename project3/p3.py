d6730c417a3c589a56972e87e7bda8f94d5ed636
Dylan Burton <burtond@uw.edu>
2020-12-05 16:42:10 -0700

    parse request

--- project3/p3.py
+++ project3/p3.py
@@ -1,4 +1,6 @@
 import sys
+import threading, _thread
+import socket
 
 
 """
@@ -45,50 +47,107 @@ Two flows:
 
 
 """
+HOST = '127.0.0.1'
+HOST_NAME = "[Proxy server]"
 
 def run():
     if len(sys.argv) != 2:
-        print("Usage: python p3.py <port_num>")
+        logp("Usage: python p3.py <port_num>")
         return
     
     try:
         PORT_NUM = int(sys.argv[1])
     except:
-        print("Usage: python p3.py <port_num>")
+        logp("Usage: python p3.py <port_num>")
         return
 
     
     # bind to socket, just like p1
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT_NUM))
+        s.listen()
+        logp("Binded on host " + str(HOST) + " on port " + str(PORT_NUM))
+        conn, addr = s.accept()
+        with conn:
+            while True:
+                """
+                # TODO: make sure connection obj works as we think and a port won't be doubly used. might need to maintain a global list of ports
+                if new request:
+                    threading.thread or whatever handle_request(connection_object, req)
+                    thread( funct_name, args=[]) something like this
+        
+                """
+                res = conn.recv(1024)
+                print(res.decode().split('\n')[0])
+                handle_request(res, conn)
 
-    while True:
-        # Listen for requests, spawn a new thread for each one
 
-        """
-        # TODO: make sure connection obj works as we think and a port won't be doubly used. might need to maintain a global list of ports
-        if new request:
-            threading.thread or whatever handle_request(connection_object, req)
-            thread( funct_name, args=[]) something like this
-
-        """
-
-def handle_request(connection_obj, req):
-    # host, req_type, req_addr, additional fields as necessary = parse_request(req)
-
-    # detect type of request, handle based on type
-    if connect is True:
-        handle_connect(connection_obj)
-        connection_obj.close()
+def handle_request(res, conn):
+    host, port, user_agent, req_type = parse_request(res)
+    if req_type == "CONNECT":
+        handle_connect(host, port, user_agent, req_type, conn)
+        conn.close()
     else:
-        create tcp to destination connection obj
-        handle_non_connect(connection_obj, tcp to destination obj) # doesn't return until done
-        connection_obj.close()
-        tcp_to_dest.close()
-
+        handle_non_connect(host, port, user_agent, req_type, conn)
+        conn.close()
     pass
 
-def parse_request(req):
+
+# Takes in data from a CONNECT or non-CONNECT and removes
+# - keep-alive
+# Returns the host, port, user_agent, req_type
+def parse_request(res):
+    logp("Parsing request")
+    # split the first line into spaces and get the host + port
+    res_decoded = res.decode()
+    split = res_decoded.split(' ')
+
+    req_type = split[0]
+    host = split[1] # could contain port
+
+    # If port is not there, use 80. If https:// is present, use 443
+    if host.find(':') == -1:
+        port = 80
+        if res_decoded.find('https://') != -1:
+            port = 443
+    else:
+        split_hp = res_decoded.split(':')
+        host = split_hp[0]
+        port = res_decoded.split(':')[1]
+
+    user_agent = res_decoded.split('User-Agent: ')[1].split("\r\n")[0]
+
+    logp("Returning from parse request " + host  + " " + port + " " + user_agent + " " + req_type)
+    return (host, port, user_agent, req_type)
+    # return host, port
+
+def logp(str):
+    print(HOST_NAME + " " + str)
+
+def build_http(host, port, user_agent, req_type, opt_msg):
+    logp("Building http send " + host + " " + port)
+    res = req_type + " " + host + ":" + port + " " + "HTTP/1.1\r\n"
+    res += user_agent + "\r\n"
+    res += "Proxy-Connection: close\r\n"
+    res += "Connection: close\r\n"
+    res += "Host: " + host + ":" + port + "\r\n"
+    res += opt_msg + "\r\n"
     pass
     # return host, req_type, additional fields as necessary
 
+def handle_non_connect(host, port, user_agent, req_type, conn):
+    logp("handling non-connect" + host + " " + port)
+    # These are all connection objects
+    # browser_to_proxy = establish or
+    # pass in from caller function)
 
-run()
+    # while True:
+    pass
+
+def handle_connect(host, port, user_agent, req_type, conn):
+    pass
+
+if __name__ == "__main__":
+    run()
+
+
