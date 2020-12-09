cc4b0e5618f0e19df0213fef7f2336808f50d0cd
Stefan Layanto <stef.layanto@gmail.com>
2020-12-09 09:30:01 +0700

    temp

--- project3/p3.py
+++ project3/p3.py
@@ -2,6 +2,7 @@ import sys
 import threading, _thread
 from bitstring import BitArray
 import socket
+import asyncio
 
 
 """
@@ -194,87 +195,96 @@ def handle_non_connect(host, port, http_msg, conn):
         s.close()
         conn.close()
 
+# handles msg async
+def response_handler(conn_recv, conn_send):
+    while True:
+        try:
+            resp = conn_recv.recv(1024)
+            if resp:
+                conn_send.send(resp)
+            else:
+                print("Breaking due to nothing recieved")
+                conn_send.close()
+                break
+        except Exception as e:
+            print("Breaking due to exception:",e)
+            conn_send.close()
+            break
+    return False
+
 
 def handle_connect(host, port, http_msg, conn):
 
-    return None
+    # return None
     # host = host.split(":")[0]
     # port = 80
     print("handling connect")
     # These are all connection objects
     browser_to_proxy = conn
-    browser_to_proxy.settimeout(2)
+    # browser_to_proxy.settimeout(2)
     proxy_to_dest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    proxy_to_dest.settimeout(2)
+    # proxy_to_dest.settimeout(2)
     try:
         proxy_to_dest.connect((host, port))
     except Exception as e:
         print("Unable to establish connection with host: {} port: {} exception: {}".format(host, port, e))
-        resp = """HTTP/1.0 502 BAD GATEWAY
-        Content-Type: text/html
-
-
-
-        """
+        resp = "HTTP/1.0 502 BAD GATEWAY\r\nContent-Type: text/html\r\n\r\n"
         browser_to_proxy.send(resp.encode('utf-8'))
         return None
-    # else:
+    
     print("Established connection with host")
     
     # ACK the browser
-    resp = """HTTP/1.0 200 OK
-    Content-Type: text/html
-
-
-
-    """
+    resp = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"
     browser_to_proxy.send(resp.encode('utf-8'))
 
-    # Forward request to dest
-    # print("PENIS\n<<<", http_msg, ">>>")
+    # Forward request to des
     proxy_to_dest.send(http_msg.encode('utf-8'))
-    
-    
 
+    _thread.start_new_thread(response_handler, (browser_to_proxy, proxy_to_dest))
+    _thread.start_new_thread(response_handler, (proxy_to_dest, browser_to_proxy))
+    # response_handler(browser_to_proxy, proxy_to_dest)
+    # response_handler(proxy_to_dest, browser_to_proxy)
+    
+    # print("Connection terminated.")
+    # browser_to_proxy.close()
+    # proxy_to_dest.close()
+    return None
     while browser_to_proxy and proxy_to_dest:
-        # print("Connect setup")
-        # if new_data_from_browser:
-        #     proxy_to_dest.send()
+        forward = response_handler(browser_to_proxy, proxy_to_dest)
+        backward = response_handler(proxy_to_dest, browser_to_proxy)
+        # if forward 
+        # print("Waiting on browser")
+        # try:
+        #     while True:
+        #         browser_response = browser_to_proxy.recv(1024)
+        #         if browser_response:
+        #             # Pull header
+        #             # print(browser_response.decode(encoding='ASCII', errors='ignore'))
+        #             # print("--------------------------")
+        #             # print(browser_response.decode(encoding='UTF-8', errors='ignore'))
+        #             # print(browser_response.decode(errors="ignore"))
+        #             proxy_to_dest.send(browser_response)
+        #         else:
+        #             print("Empty response from browser")
+        #             break
+        # except:
+        #     print("nothing received from browser")
 
-        # if new_data_from_proxy:
-        #     browser_to_proxy.send()
-        
-        print("Waiting on browser")
-        try:
-            while True:
-                browser_response = browser_to_proxy.recv(1024)
-                if browser_response:
-                    # Pull header
-                    # print(browser_response.decode(encoding='ASCII', errors='ignore'))
-                    # print("--------------------------")
-                    # print(browser_response.decode(encoding='UTF-8', errors='ignore'))
-                    # print(browser_response.decode(errors="ignore"))
-                    proxy_to_dest.send(browser_response)
-                else:
-                    print("Empty response from browser")
-                    break
-        except:
-            print("nothing received from browser")
-
-        print("Waiting on dest")
-        try:
-            while True:
-                dest_response = proxy_to_dest.recv(1024)
-                # logp("Response from server " + response.decode())
-                if dest_response: # Forward to client
-                    # print(dest_response.decode(errors="ignore"))
-                    browser_to_proxy.send(dest_response)
-                else:
-                    print("Empty response from dest")
-                    break
-        except:
-            print("nothing received from dest")
-        # pass
+        # print("Waiting on dest")
+        # try:
+        #     while True:
+        #         dest_response = proxy_to_dest.recv(1024)
+        #         # logp("Response from server " + response.decode())
+        #         if dest_response: # Forward to client
+        #             # print(dest_response.decode(errors="ignore"))
+        #             browser_to_proxy.send(dest_response)
+        #         else:
+        #             print("Empty response from dest")
+        #             break
+        # except:
+        #     print("nothing received from dest")
+        # # pass
     
     # CLEANUP
 
